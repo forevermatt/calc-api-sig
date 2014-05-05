73a692fb6949b8da252bc4c7c76b79987cf5de40
Matt Henderson <matt_henderson@sil.org>
2014-05-05 14:14:32 -0400

    Created basically-empty class for actually doing the work.

---
+++ src/HmacSigner.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CalcApiSig;
+
+/**
+ * Helper class to simplify generating a signature for APIs that use HMAC.
+ *
+ * @author forevermatt
+ */
+class HmacSigner
+{
+    public static function CalcApiSig($apiKey, $sharedSecret, $time = NULL)
+    {
+        // TEMP
+        return '';
+    }
+}
